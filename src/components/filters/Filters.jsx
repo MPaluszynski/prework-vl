29947ed8c1e65cb4d4c6297c06fca4ec9237cc2c
Maciej Paluszynski <macpalus@gmail.com>
2022-01-10 23:40:05 +0100

    Implementing reausable components structure

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -1,5 +1,17 @@
 import "./filters-styles.css";
 
 export const Filters = () => {
-  return <div className="filters"></div>;
+  return (
+    <div className="filters">
+      {" "}
+      <div>
+        <input
+          className="nav-input"
+          type="text"
+          placeholder="Znajdź usługę..."
+          // onChange={(e) => updateField(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 };
