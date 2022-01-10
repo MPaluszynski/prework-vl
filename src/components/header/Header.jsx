d9fbb4eeb29bcacbec4ac1335b134dfa17d897f1
Maciej Paluszynski <macpalus@gmail.com>
2022-01-10 23:20:16 +0100

    Finalization of Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,9 @@
 import "./header-styles.css";
 
 export const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <h1>Characters</h1>
+    </div>
+  );
 };
