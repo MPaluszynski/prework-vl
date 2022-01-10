90ae2d31ea945559936142fbbcd5f6aec2658598
Maciej Paluszynski <macpalus@gmail.com>
2022-01-10 22:51:46 +0100

    Expanding project structure. Implementing basic styles composition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div>AAA</div>;
-}
+import "./styles/main-styles.css";
+
+import { Header } from "./components/header/Header.jsx";
+import { Data } from "./components/data/Data.jsx";
+import { Filters } from "./components/filters/Filters.jsx";
+import { Footer } from "./components/footer/Footer.jsx";
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      <Filters />
+      <Data />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
