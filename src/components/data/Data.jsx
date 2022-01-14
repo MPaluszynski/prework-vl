59d943d0b6fdf7b43df194536b2cdc04d2681033
Maciej Paluszynski <macpalus@gmail.com>
2022-01-14 22:53:32 +0100

    Implementing grid lines schema for Data component

--- src/components/data/Data.jsx
+++ src/components/data/Data.jsx
@@ -1,5 +1,22 @@
 import "./data-styles.css";
 
 export const Data = () => {
-  return <div className="data"></div>;
+  return (
+    <div className="data">
+      <div className="data__table">
+        <div className="data__column">1</div>
+        <div className="data__column">2</div>
+        <div className="data__column">3</div>
+        <div className="data__column">4</div>
+        <div className="data__column">5</div>
+        <div className="data__column">6</div>
+        <div className="data__column">1</div>
+        <div className="data__column">2</div>
+        <div className="data__column">3</div>
+        <div className="data__column">4</div>
+        <div className="data__column">5</div>
+        <div className="data__column">6</div>
+      </div>
+    </div>
+  );
 };
