f78d8d0b084ed753cd1fc61b7627bbfb09e7ffc9
Maciej Paluszynski <macpalus@gmail.com>
2022-01-17 00:03:13 +0100

    Implementing data import for multiselect filtration mechanism

--- src/components/filters/Filters.jsx
+++ src/components/filters/Filters.jsx
@@ -3,27 +3,13 @@ import { Input } from "../_reusableComponents/input/Input";
 import { Dropdown } from "../_reusableComponents/dropdown/Dropdown";
 import { Button } from "../_reusableComponents/button/Button";
 
+import {
+  statusArray,
+  speciesArray,
+  homeworldsArray,
+} from "./../../data/arrays.js";
+
 export const Filters = (props) => {
-  // Do usunięcia po ogarnięciu fetchowania
-  const speciesArray = [
-    { name: "Human", id: 1 },
-    { name: "Droid", id: 2 },
-    { name: "Wookie", id: 3 },
-    { name: "Rodian", id: 4 },
-    { name: "Hutt", id: 5 },
-    { name: "Yoda's species", id: 6 },
-  ];
-  const homeworldsArray = [
-    { name: "Tatooine", id: 1 },
-    { name: "Alderaan", id: 2 },
-    { name: "Yavin IV", id: 3 },
-  ];
-
-  const statusArray = [
-    { name: "Active", id: 1 },
-    { name: "Deactivated", id: 2 },
-  ];
-
   return (
     <div className="filters">
       <div className="filters__inputs">
---
+++ src/data/arrays.js
@@ -0,0 +1,497 @@
+export const statusArray = [
+  { name: "Active", id: 1 },
+  { name: "Deactivated", id: 2 },
+  { name: "All", id: 3 },
+];
+
+export const speciesArray = [
+  {
+    name: "Human",
+    id: 1,
+    url: "https://swapi.dev/api/species/1/",
+  },
+  {
+    name: "Droid",
+    id: 2,
+    url: "https://swapi.dev/api/species/2/",
+  },
+  {
+    name: "Wookie",
+    id: 3,
+    url: "https://swapi.dev/api/species/3/",
+  },
+  {
+    name: "Rodian",
+    id: 4,
+    url: "https://swapi.dev/api/species/4/",
+  },
+  {
+    name: "Hutt",
+    id: 5,
+    url: "https://swapi.dev/api/species/5/",
+  },
+  {
+    name: "Yoda's species",
+    id: 6,
+    url: "https://swapi.dev/api/species/6/",
+  },
+  {
+    name: "Trandoshan",
+    id: 7,
+    url: "https://swapi.dev/api/species/7/",
+  },
+  {
+    name: "Mon Calamari",
+    id: 8,
+    url: "https://swapi.dev/api/species/8/",
+  },
+  {
+    name: "Ewok",
+    id: 9,
+    url: "https://swapi.dev/api/species/9/",
+  },
+  {
+    name: "Sullustan",
+    id: 10,
+    url: "https://swapi.dev/api/species/10/",
+  },
+  {
+    name: "Neimodian",
+    id: 11,
+    url: "https://swapi.dev/api/species/11/",
+  },
+  {
+    name: "Gungan",
+    id: 12,
+    url: "https://swapi.dev/api/species/12/",
+  },
+  {
+    name: "Toydarian",
+    id: 13,
+    url: "https://swapi.dev/api/species/13/",
+  },
+  {
+    name: "Dug",
+    id: 14,
+    url: "https://swapi.dev/api/species/14/",
+  },
+  {
+    name: "Twi'lek",
+    id: 15,
+    url: "https://swapi.dev/api/species/15/",
+  },
+  {
+    name: "Aleena",
+    id: 16,
+    url: "https://swapi.dev/api/species/16/",
+  },
+  {
+    name: "Vulptereen",
+    id: 17,
+    url: "https://swapi.dev/api/species/17/",
+  },
+  {
+    name: "Xexto",
+    id: 18,
+    url: "https://swapi.dev/api/species/18/",
+  },
+  {
+    name: "Toong",
+    id: 19,
+    url: "https://swapi.dev/api/species/19/",
+  },
+  {
+    name: "Cerean",
+    id: 20,
+    url: "https://swapi.dev/api/species/20/",
+  },
+  {
+    name: "Nautolan",
+    id: 21,
+    url: "https://swapi.dev/api/species/21/",
+  },
+  {
+    name: "Zabrak",
+    id: 22,
+    url: "https://swapi.dev/api/species/22/",
+  },
+  {
+    name: "Tholothian",
+    id: 23,
+    url: "https://swapi.dev/api/species/23/",
+  },
+  {
+    name: "Iktotchi",
+    id: 24,
+    url: "https://swapi.dev/api/species/24/",
+  },
+  {
+    name: "Quermian",
+    id: 25,
+    url: "https://swapi.dev/api/species/25/",
+  },
+  {
+    name: "Kel Dor",
+    id: 26,
+    url: "https://swapi.dev/api/species/26/",
+  },
+  {
+    name: "Chagrian",
+    id: 27,
+    url: "https://swapi.dev/api/species/27/",
+  },
+  {
+    name: "Geonosian",
+    id: 28,
+    url: "https://swapi.dev/api/species/28/",
+  },
+  {
+    name: "Mirialan",
+    id: 29,
+    url: "https://swapi.dev/api/species/29/",
+  },
+  {
+    name: "Clawdite",
+    id: 30,
+    url: "https://swapi.dev/api/species/30/",
+  },
+  {
+    name: "Besalisk",
+    id: 31,
+    url: "https://swapi.dev/api/species/31/",
+  },
+  {
+    name: "Kaminoan",
+    id: 32,
+    url: "https://swapi.dev/api/species/32/",
+  },
+  {
+    name: "Skakoan",
+    id: 33,
+    url: "https://swapi.dev/api/species/33/",
+  },
+  {
+    name: "Muun",
+    id: 34,
+    url: "https://swapi.dev/api/species/34/",
+  },
+  {
+    name: "Togruta",
+    id: 35,
+    url: "https://swapi.dev/api/species/35/",
+  },
+  {
+    name: "Kaleesh",
+    id: 36,
+    url: "https://swapi.dev/api/species/36/",
+  },
+  {
+    name: "Pau'an",
+    id: 37,
+    url: "https://swapi.dev/api/species/37/",
+  },
+];
+
+export const homeworldsArray = [
+  {
+    name: "Tatooine",
+    id: 1,
+    url: "https://swapi.dev/api/planets/1/",
+  },
+  {
+    name: "Alderaan",
+
+    id: 2,
+    url: "https://swapi.dev/api/planets/2/",
+  },
+  {
+    name: "Yavin IV",
+    id: 3,
+    url: "https://swapi.dev/api/planets/3/",
+  },
+  {
+    name: "Hoth",
+    id: 4,
+    url: "https://swapi.dev/api/planets/4/",
+  },
+  {
+    name: "Dagobah",
+    id: 5,
+    url: "https://swapi.dev/api/planets/5/",
+  },
+  {
+    name: "Bespin",
+    id: 6,
+    url: "https://swapi.dev/api/planets/6/",
+  },
+  {
+    name: "Endor",
+    id: 7,
+    url: "https://swapi.dev/api/planets/7/",
+  },
+  {
+    name: "Naboo",
+    id: 8,
+    url: "https://swapi.dev/api/planets/8/",
+  },
+  {
+    name: "Coruscant",
+    id: 9,
+    url: "https://swapi.dev/api/planets/9/",
+  },
+  {
+    name: "Kamino",
+    id: 10,
+    url: "https://swapi.dev/api/planets/10/",
+  },
+  {
+    name: "Geonosis",
+    id: 11,
+    url: "https://swapi.dev/api/planets/11/",
+  },
+  {
+    name: "Utapau",
+    id: 12,
+    url: "https://swapi.dev/api/planets/12/",
+  },
+  {
+    name: "Mustafar",
+    id: 13,
+    url: "https://swapi.dev/api/planets/13/",
+  },
+  {
+    name: "Kashyyyk",
+    id: 14,
+    url: "https://swapi.dev/api/planets/14/",
+  },
+  {
+    name: "Polis Massa",
+    id: 15,
+    url: "https://swapi.dev/api/planets/15/",
+  },
+  {
+    name: "Mygeeto",
+    id: 16,
+    url: "https://swapi.dev/api/planets/16/",
+  },
+  {
+    name: "Felucia",
+    id: 17,
+    url: "https://swapi.dev/api/planets/17/",
+  },
+  {
+    name: "Cato Neimoidia",
+    id: 18,
+    url: "https://swapi.dev/api/planets/18/",
+  },
+  {
+    name: "Saleucami",
+    id: 19,
+    url: "https://swapi.dev/api/planets/19/",
+  },
+  {
+    name: "Stewjon",
+    id: 20,
+    url: "https://swapi.dev/api/planets/20/",
+  },
+  {
+    name: "Eriadu",
+    id: 21,
+    url: "https://swapi.dev/api/planets/21/",
+  },
+  {
+    name: "Corellia",
+    id: 22,
+    url: "https://swapi.dev/api/planets/22/",
+  },
+  {
+    name: "Rodia",
+    id: 23,
+    url: "https://swapi.dev/api/planets/23/",
+  },
+  {
+    name: "Nal Hutta",
+    id: 24,
+    url: "https://swapi.dev/api/planets/24/",
+  },
+  {
+    name: "Dantooine",
+    id: 25,
+    url: "https://swapi.dev/api/planets/25/",
+  },
+  {
+    name: "Bestine IV",
+    id: 26,
+    url: "https://swapi.dev/api/planets/26/",
+  },
+  {
+    name: "Ord Mantell",
+    id: 27,
+    url: "https://swapi.dev/api/planets/27/",
+  },
+  {
+    name: "unknown",
+    id: 28,
+    url: "https://swapi.dev/api/planets/28/",
+  },
+  {
+    name: "Trandosha",
+    id: 29,
+    url: "https://swapi.dev/api/planets/29/",
+  },
+  {
+    name: "Socorro",
+    id: 30,
+    url: "https://swapi.dev/api/planets/30/",
+  },
+  {
+    name: "Mon Cala",
+    id: 31,
+    url: "https://swapi.dev/api/planets/31/",
+  },
+  {
+    name: "Chandrila",
+    id: 32,
+    url: "https://swapi.dev/api/planets/32/",
+  },
+  {
+    name: "Sullust",
+    id: 33,
+    url: "https://swapi.dev/api/planets/33/",
+  },
+  {
+    name: "Toydaria",
+    id: 34,
+    url: "https://swapi.dev/api/planets/34/",
+  },
+  {
+    name: "Malastare",
+    id: 35,
+    url: "https://swapi.dev/api/planets/35/",
+  },
+  {
+    name: "Dathomir",
+    id: 36,
+    url: "https://swapi.dev/api/planets/36/",
+  },
+  {
+    name: "Ryloth",
+    id: 37,
+    url: "https://swapi.dev/api/planets/37/",
+  },
+  {
+    name: "Aleen Minor",
+    id: 38,
+    url: "https://swapi.dev/api/planets/38/",
+  },
+  {
+    name: "Vulpter",
+    id: 39,
+    url: "https://swapi.dev/api/planets/39/",
+  },
+  {
+    name: "Troiken",
+    id: 40,
+    url: "https://swapi.dev/api/planets/40/",
+  },
+  {
+    name: "Tund",
+    id: 41,
+    url: "https://swapi.dev/api/planets/41/",
+  },
+  {
+    name: "Haruun Kal",
+    id: 42,
+    url: "https://swapi.dev/api/planets/42/",
+  },
+  {
+    name: "Cerea",
+    id: 43,
+    url: "https://swapi.dev/api/planets/43/",
+  },
+  {
+    name: "Glee Anselm",
+    id: 44,
+    url: "https://swapi.dev/api/planets/44/",
+  },
+  {
+    name: "Iridonia",
+    id: 45,
+    url: "https://swapi.dev/api/planets/45/",
+  },
+  {
+    name: "Tholoth",
+    id: 46,
+    url: "https://swapi.dev/api/planets/46/",
+  },
+  {
+    name: "Iktotch",
+    id: 47,
+    url: "https://swapi.dev/api/planets/47/",
+  },
+  {
+    name: "Quermia",
+    id: 48,
+    url: "https://swapi.dev/api/planets/48/",
+  },
+  {
+    name: "Dorin",
+    id: 49,
+    url: "https://swapi.dev/api/planets/49/",
+  },
+  {
+    name: "Champala",
+    id: 50,
+    url: "https://swapi.dev/api/planets/50/",
+  },
+  {
+    name: "Mirial",
+    id: 51,
+    url: "https://swapi.dev/api/planets/51/",
+  },
+  {
+    name: "Serenno",
+    id: 52,
+    url: "https://swapi.dev/api/planets/52/",
+  },
+  {
+    name: "Concord Dawn",
+    id: 53,
+    url: "https://swapi.dev/api/planets/53/",
+  },
+  {
+    name: "Zolan",
+    id: 54,
+    url: "https://swapi.dev/api/planets/54/",
+  },
+  {
+    name: "Ojom",
+    id: 55,
+    url: "https://swapi.dev/api/planets/55/",
+  },
+  {
+    name: "Skako",
+    id: 56,
+    url: "https://swapi.dev/api/planets/56/",
+  },
+  {
+    name: "Muunilinst",
+    id: 57,
+    url: "https://swapi.dev/api/planets/57/",
+  },
+  {
+    name: "Shili",
+    id: 58,
+    url: "https://swapi.dev/api/planets/58/",
+  },
+  {
+    name: "Kalee",
+    id: 59,
+    url: "https://swapi.dev/api/planets/59/",
+  },
+  {
+    name: "Umbara",
+    id: 60,
+    url: "https://swapi.dev/api/planets/60/",
+  },
+];
